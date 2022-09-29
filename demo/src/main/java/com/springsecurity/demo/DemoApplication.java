b32387768d848c6d736f6bbe30dbdb58d0c37277
Aljay Pascual <aljay.pascual@norimaconsulting.com>
2022-09-29 13:27:42 +0800

    first commit

---
+++ demo/src/main/java/com/springsecurity/demo/DemoApplication.java
@@ -0,0 +1,16 @@
+package com.springsecurity.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.springsecurity.demo.Config.RsaKeyProperties;
+@EnableConfigurationProperties(RsaKeyProperties.class)
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
